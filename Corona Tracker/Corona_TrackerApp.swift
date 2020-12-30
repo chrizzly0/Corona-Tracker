f8bd7ead74dbc175e4e41604f819029cc9bef1d1
Chrizzly <chrissikle@outlook.de>
2020-12-30 14:31:13 +0100

    Initial Commit

---
+++ Corona Tracker/Corona_TrackerApp.swift
@@ -0,0 +1,14 @@
+//Corona_TrackerApp.swift
+//
+//Created by Chrizzly on 30.12.20.
+
+import SwiftUI
+
+@main
+struct Corona_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
